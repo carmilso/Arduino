13e1837ccf87e0b195e42460f1acfafa6d0dda1b
Carlos Millán Soler <carmilso@inf.upv.es>
2015-03-06 10:58:19 +0100

    Añadida opción DEBUG

--- pir/pir.ino
+++ pir/pir.ino
@@ -1,6 +1,7 @@
 #define luz 4
 #define pir 9
 
+#define DEBUG 0
 
 int val;
 int countT;
@@ -10,17 +11,25 @@ void setup(){
   pinMode(luz, OUTPUT);
   digitalWrite(luz, HIGH);
   pinMode(pir, INPUT);
+
   countT = 0;
   countF = 0;
+
   delay(3000);
-  Serial.begin(9600);
+
+  if(DEBUG)
+    Serial.begin(9600);
 }
 
 void loop(){
   val = digitalRead(pir);
-  Serial.print("Valor leido -> ");
-  Serial.println(val);
-  Serial.println();
+
+  if(DEBUG){
+    Serial.print("Valor leido -> ");
+    Serial.println(val);
+    Serial.println();
+  }
+
   if(val==1){
     countT++;
     countF = 0;
@@ -29,6 +38,7 @@ void loop(){
     countT = 0;
     countF++;
   }
+  
   if(countT == 3)
     digitalWrite(luz, LOW);
   if(countF >= 30)
